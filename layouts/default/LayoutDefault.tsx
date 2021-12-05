cd9d0d73768ab5dd05226bc63861acbf58af336b
Sam K <hello@samk.dev>
2021-12-05 04:59:41 +0100

    🚧 added support for postcss & tailwind nesting

--- layouts/default/LayoutDefault.tsx
+++ layouts/default/LayoutDefault.tsx
@@ -1,12 +1,14 @@
-import style from './LayoutDefault.module.css';
+import css from './LayoutDefault.module.css';
 import { LayoutDefaultProps } from '../types';
 
 const LayoutDefault = ({ children }: LayoutDefaultProps) => {
   return (
     <>
-      <main className={style.layout}>
-        <h1>Default Layout</h1>
-        {children}
+      <main className={css.layout}>
+        <div className={css.layoutContent}>
+          <h1>Default Layout</h1>
+          {children}
+        </div>
       </main>
     </>
   );
